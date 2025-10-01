4d82d9c66aa25dab4c3beb6c1272159a78c70a4a
pedromarinhoo <pedromto3@gmail.com>
2025-10-01 10:03:39 -0300

    initial frame

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
 import tkinter as tk
 
+# configurando a janela principal
 window = tk.Tk()
-window.title("Teste")
-window.geometry("1000x1000+20+20")
+window.title("CRUD em Python + Tkinter")
+window.geometry("2000x2000")
+window.configure(bg="#000")
+window.attributes('-zoomed', True)
 
-lblMsg = tk.Label(window, text="Teste de janela")
-lblMsg.pack()
+# dividindo os frames da aplicação
+upperFrame = tk.Frame(window, width=400, height=50, bg="#338BF2", relief="flat")
+upperFrame.grid(row=0, column=0)
+
+bottomFrame = tk.Frame(window, width=400, height=2000, bg="#FFFFFF")
+bottomFrame.grid(row=1, column=0, sticky=tk.NSEW, padx=0, pady=1)
+
+rightFrame = tk.Frame(window, width=1000, height=1000, bg="#FFFFFF", relief="flat")
+rightFrame.grid(row=0, column=1, sticky=tk.NSEW, padx=1, pady=1)
+
+
+
+lblMsg = tk.Label(window, text="CRUDs em Python")
 
 window.mainloop()
